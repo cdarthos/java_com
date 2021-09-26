ae55e6d1fb26d2ed06e10f4c2a039d4e906b1f3f
cdarthos <Azerty4221>
2021-09-26 21:49:14 +0200

    test 2

---
+++ socket_server/src/ClientConnect.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class ClientConnect {
+    public static void main(String[] args) {
+        Socket sock;
+        String machineServer = "127.0.0.1";
+        int port = 5555;
+
+        try {
+            sock = new Socket(machineServer,port);
+            InputStream is = sock.getInputStream();
+
+            byte[] tabloCli = new byte[5];
+            int recu = is.read(tabloCli);
+
+            sock.close();
+
+            System.out.println(recu);
+        }
+        catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+}
--- socket_server/src/ServerConnect.java
+++ socket_server/src/ServerConnect.java
@@ -1,11 +1,26 @@
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerConnect {
     public static void main(String[] arges) {
         ServerSocket srv;
 
         try {
+            srv = new ServerSocket(5555) ;
+            Socket connexion = srv.accept();
 
+            OutputStream os = connexion.getOutputStream();
+
+            byte[] tabloServ = { 1,2,3,4,5};
+
+            os.write(tabloServ);
+
+            connexion.close();
+        }
+        catch (IOException ex) {
+            System.out.println(ex);
         }
     }
 }
